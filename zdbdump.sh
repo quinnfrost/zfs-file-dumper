dd9eb8870671d574bf7237004d410bb08dbce116
quinnfrost <892605600@qq.com>
2022-10-21 21:44:27 +0800

    add timing code

--- zdbdump.sh
+++ zdbdump.sh
@@ -17,13 +17,17 @@ CHANGE_TIME=""
 CREATION_TIME=""
 DUMP_OFFSET="0:2f2ea8000:1000"
 
-$ZDB -e -AAA -ddddd "${POOLNAME}/${DATASET}" $OBJECT_ID
+echo "[$(date "+%Y%m%d %H:%M:%S")] Start processing $OBJECT_ID"
+START_TIME=$(date +%s%3N)
 
-$ZDB -R -e $POOLNAME "${DUMP_OFFSET}:r" > ./testdumpfile.md
+$ZDB -e -AAA -ddddd "${POOLNAME}/${DATASET}" $OBJECT_ID 
+
+$ZDB -R -e $POOLNAME "${DUMP_OFFSET}:r" > $FILE_PATH
 
 truncate --size=$SIZE $FILE_PATH
 
 echo $1 $2 
 echo $@
 
-
+ELAPSED_TIME=$(expr $(date +%s%3N) - $START_TIME)
+echo "Command finished in $ELAPSED_TIME milliseconds"
